6fa7c16a897d9caa162f4b7f0dc04364ee62841a
LunarPhoenix-Judith <judith.oostrom@hva.nl>
2022-10-14 09:28:53 +0200

    sprint-3-def

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,7 @@ document
   .addEventListener("click", () => {
     document.querySelector(".time-table").classList.toggle("time-table-active")
   })
+
+document.querySelector(".early").addEventListener("click", () => {
+  document.querySelector(".next").classList.toggle("next-active")
+})
